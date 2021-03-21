21103f2823a71a74542a6e4b93bed0eeb6d0b941
Jerry Robertson <jerry.r.robertson1@gmail.com>
2021-03-21 10:34:52 -0500

    Fibo update

--- Problem2.js
+++ Problem2.js
@@ -9,30 +9,7 @@ const Fibonacci = (limit) => {
 };
 
 console.log(
-  Fibonacci(4000000).reduce((total, num) =>
+  Fibonacci(40000000).reduce((total, num) =>
     num % 2 === 0 ? total + num : total
   )
 );
-
-function calculateAmount(prices) {
-  // Write your code here
-  let total = 0;
-  let prev = 0;
-  for (const x of prices) {
-    if (prices[0] === x) {
-      total += x;
-      prev = x;
-    } else {
-      let temp = x - prev;
-      if (temp < 1) {
-        total += 0;
-        prev = x;
-      } else {
-        total += temp;
-        prev = temp;
-      }
-      console.log(total);
-    }
-  }
-  return total;
-}
